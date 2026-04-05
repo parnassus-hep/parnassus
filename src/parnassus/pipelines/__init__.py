f2990bfdeb8bff4040fe4e9a92a839aa4427dee2
Dmitrii Kobylianskii <kobad2@gmail.com>
2026-04-06 00:29:38 +0300

    Cleanup

--- src/parnassus/pipelines/__init__.py
+++ src/parnassus/pipelines/__init__.py
@@ -3,20 +3,10 @@
 from .cluster import JetClusteringPipeline
 from .generate import GenerationPipeline, generate
 from .isolation import IsolationPipeline
-from .torch_delphes import (
-    get_torch_delphes_accessors,
-    tensor_to_gen_events,
-    tensor_to_gen_particle_collection,
-    write_torch_delphes_output,
-)
 
 __all__ = [
     "GenerationPipeline",
     "IsolationPipeline",
     "JetClusteringPipeline",
     "generate",
-    "get_torch_delphes_accessors",
-    "tensor_to_gen_events",
-    "tensor_to_gen_particle_collection",
-    "write_torch_delphes_output",
 ]
